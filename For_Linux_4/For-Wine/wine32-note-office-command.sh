15a7d36dd1c6de991c1448192bcdd1a7879a9d7a
Mikl <maximalis171091@yandex.ru>
2023-02-13 19:54:52 +0300

    Update

--- For_Linux_4/For-Wine/wine32-note-office-command.sh
+++ For_Linux_4/For-Wine/wine32-note-office-command.sh
@@ -6,6 +6,11 @@ export WINEARCH=win32
 ./winetricks corefonts allfonts 
 ./winetricks msxml3 msxml4 msxml6
 ./winetricks vcrun2005 gdiplus riched30
+winetricks dotnet20 dotnet30 dotnet35 dotnet40 dotnet45
+winetricks vcrun2005 vcrun2008 vcrun2010
+winetricks d3dx9 d3dx9_28 d3dx9_36 d3dx10
+winetricks d3dxof dinput8 directmusic directplay
+winetricks directx9 directx10
 # wine ~/Загрузки/MicrosoftOffice/setup.exe
 # wine ~/.msoffice/dosdevices/c:/Program Files/Microsoft Office/Office14/winword.exe
 # wine ~/.msoffice/dosdevices/c:/Program Files/Microsoft Office/Office14/exel.exe
