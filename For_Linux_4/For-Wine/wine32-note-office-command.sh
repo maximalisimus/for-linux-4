db684a62212c7b3a3798507338700b5c7d788dc9
Mikl <maximalis171091@yandex.ru>
2023-02-21 23:19:19 +0300

    Update

--- For_Linux_4/For-Wine/wine32-note-office-command.sh
+++ For_Linux_4/For-Wine/wine32-note-office-command.sh
@@ -10,6 +10,11 @@ winetricks dotnet11 dotnet20 dotnet20sp2 dotnet30 dotnet35 dotnet40 dotnet45
 winetricks vcrun2005 vcrun2008 vcrun2010
 winetricks d3dx9 d3dx9_28 d3dx9_36 d3dx10
 winetricks d3dxof dinput8 directmusic directplay
+winetricks msftedit
+winetricks riched20
+winetricks riched30
+winetricks riched32
+winetricks vb6run
 # wine ~/Загрузки/MicrosoftOffice/setup.exe
 # wine ~/.msoffice/dosdevices/c:/Program Files/Microsoft Office/Office14/winword.exe
 # wine ~/.msoffice/dosdevices/c:/Program Files/Microsoft Office/Office14/exel.exe
